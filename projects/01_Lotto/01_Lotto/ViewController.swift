52a281c408ddc17b5efef348c4b7db800947056e
yoee <yoee.kim@gmail.com>
2019-03-19 19:44:36 +0900

    initial commit

---
+++ projects/01_Lotto/01_Lotto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01_Lotto
+//
+//  Created by 김나용 on 19/03/2019.
+//  Copyright © 2019 Yoee Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
